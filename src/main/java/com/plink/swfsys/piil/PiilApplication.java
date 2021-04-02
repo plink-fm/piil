291f9445110a72db0edf3fd79271d70d10ed122b
plink <palooka2@hotmail.com>
2021-04-02 11:52:53 -0700

    Initial commit.

---
+++ src/main/java/com/plink/swfsys/piil/PiilApplication.java
@@ -0,0 +1,13 @@
+package com.plink.swfsys.piil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PiilApplication.class, args);
+	}
+
+}
